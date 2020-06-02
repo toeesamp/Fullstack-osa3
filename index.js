ad3eeca6be8104a5132f91b546665ce49c3f9f09
Tommi Sampo <tommi.sampo@kela.fi>
2020-06-02 14:52:06 +0300

    3.4: puhelinluettelon backend step4

--- index.js
+++ index.js
@@ -47,6 +47,13 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
+})
+
 app.get('/info', (request, response) => {
     console.log('request ',request.headers)
     console.log('response ',response)
