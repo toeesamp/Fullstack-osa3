bccef854aa4d020bc73beeec26ffc22ecc5ff2b7
Tommi Sampo <tommi.sampo@kela.fi>
2020-06-02 14:44:59 +0300

    3.3: puhelinluettelon backend step3

--- index.js
+++ index.js
@@ -29,21 +29,31 @@ let persons = [
     }
 ]
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello!</h1>')
+app.get('/', (request, response) => {
+    response.send('<h1>Hello!</h1>')
 })
 
-app.get('/api/persons', (req, res) => {
-    res.json(persons)
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
 })
 
-app.get('/info', (req, res) => {
-    console.log('request ',req.headers)
-    console.log('response ',res)
-    res.type('html')
-    res.write(`<p>Phonebook has info for ${persons.length - 1} people</p>`)
-    res.write(`<p> ${new Date()} </p>`)
-    res.end()
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.get('/info', (request, response) => {
+    console.log('request ',request.headers)
+    console.log('response ',response)
+    response.type('html')
+    response.write(`<p>Phonebook has info for ${persons.length - 1} people</p>`)
+    response.write(`<p> ${new Date()} </p>`)
+    response.end()
 })
 
 const PORT = 3001
