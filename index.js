3ba6abc091f81ffb3e469e9cdb7fb0103e0af8d8
Tommi Sampo <tommi.sampo@kela.fi>
2020-06-02 12:30:56 +0300

    3.1 puhelinluettelon backend step1

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-1234567",
+        "id": 1
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+    },
+    {
+        "name": "asd",
+        "number": "12",
+        "id": 5
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
