4a8c8536b94241fc7870df9a35cc04f8fcbb4371
Tommi Sampo <tommi.sampo@kela.fi>
2020-06-03 12:03:22 +0300

    3.5: puhelinluettelon backend step5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json()) 
+
 let persons = [
     {
         "name": "Arto Hellas",
@@ -29,6 +31,14 @@ let persons = [
     }
 ]
 
+const generateId = () => {
+    return getRandomInt(9001)
+}
+
+const getRandomInt = (max) => {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello!</h1>')
 })
@@ -52,6 +62,22 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
   
     response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    if (!body.name) {
+        return response.status(400).json({ 
+            error: 'name missing' 
+        })
+    }
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+    persons = persons.concat(person)
+    response.json(person)
 })
 
 app.get('/info', (request, response) => {
