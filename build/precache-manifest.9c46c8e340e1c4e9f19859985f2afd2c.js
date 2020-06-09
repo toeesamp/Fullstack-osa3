38dab2ecdb97840f8ded36b82fee45ae91357298
toeesamp <tommi.sampo@gmail.com>
2020-06-09 16:08:45 +0300

    3.11 puhelinluettelo full stack

---
+++ build/precache-manifest.9c46c8e340e1c4e9f19859985f2afd2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62b5cc42b3cc67e97164876fa1a20908",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f77ffc5d9a65ed44604c",
+    "url": "/static/css/main.073da8de.chunk.css"
+  },
+  {
+    "revision": "25ee9fd4b9873ea818b9",
+    "url": "/static/js/2.90859df1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.90859df1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f77ffc5d9a65ed44604c",
+    "url": "/static/js/main.b3a0e1db.chunk.js"
+  },
+  {
+    "revision": "24aecb121d5a1e31ca85",
+    "url": "/static/js/runtime-main.455ce434.js"
+  }
+]);
--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 
-app.use(express.json()) 
+app.use(express.json())
+app.use(express.static('build'))
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :requestBody'))
 
