81f79ddc4fc96971e45edf51ffb26bb7d8babaf7
toeesamp <tommi.sampo@gmail.com>
2020-06-10 17:03:45 +0300

    Fixed collection.ensureIndex deprecation warning

--- models/person.js
+++ models/person.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator');
 mongoose.set('useFindAndModify', false)
-//mongoose.set('useCreateIndex', true);
+mongoose.set('useCreateIndex', true);
 
 const url = process.env.MONGODB_URI
 
