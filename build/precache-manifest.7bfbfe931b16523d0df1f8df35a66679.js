d4692b5cf553fda6d9ff094d37d5f2217dd18aaf
toeesamp <tommi.sampo@gmail.com>
2020-06-10 17:45:27 +0300

    uibuild

---
+++ build/precache-manifest.7bfbfe931b16523d0df1f8df35a66679.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c761253c8e8a82272b351731213db11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e936ad02e7865e14c10",
+    "url": "/static/css/main.073da8de.chunk.css"
+  },
+  {
+    "revision": "25ee9fd4b9873ea818b9",
+    "url": "/static/js/2.90859df1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.90859df1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e936ad02e7865e14c10",
+    "url": "/static/js/main.1770ede2.chunk.js"
+  },
+  {
+    "revision": "24aecb121d5a1e31ca85",
+    "url": "/static/js/runtime-main.455ce434.js"
+  }
+]);
